79ed995a19c0c1fb88ecf2bc4dfec3a993fbe03c
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-10-05 13:24:05 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//2
+console.log('Hello World');
+
+//3
+//console.log('This is my first js file');
+
+/* If I remove a parenthesis from console.log the code throws an error */
+/* The error is: SyntaxError: missing ) after argument list */
