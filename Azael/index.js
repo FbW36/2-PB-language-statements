b250cd5ee5b1a121cc8dd58ce480836a80eda225
Zarzarius <zarzarius@gmail.com>
2020-10-05 23:46:43 +0200

    exercise done

---
+++ Azael/index.js
@@ -0,0 +1,8 @@
+console.log("Hello World!")
+
+// console.log("This is my second Javascript file")
+
+// 1. Remove a parenthesis from the first statement and run the code. Does your statement print?
+// ==> No
+// 2. Is there an error? If so, what kind of error is it?
+// ==> SyntaxError: Unexpected string
