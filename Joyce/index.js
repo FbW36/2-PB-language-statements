71f2dffbb05659165a0010829cd344a556a641c3
Joyce <puch1996@gmail.com>
2020-10-05 14:36:38 +0200

    modified

---
+++ Joyce/index.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+//console.log("This is my first js file");
+
+// Questions:
+// Remove a parenthesis from the first statement, the statement won't print.
+// There is SyntaxError: missing ) after argument list
