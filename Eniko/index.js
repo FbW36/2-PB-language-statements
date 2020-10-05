2e70c5be46e98d032cc57e5fe3efd6433284f054
Enikő Veszelka <encsooo@gmail.com>
2020-10-05 14:03:33 +0200

    exercise done

---
+++ Eniko/index.js
@@ -0,0 +1,18 @@
+console.log(Hello World!);
+
+//console.log("This is my first JavaScript file.");
+
+//console.log(Hello World!);
+//^^^^^
+
+// SyntaxError: missing ) after argument list
+//     at wrapSafe (internal/modules/cjs/loader.js:1053:16)
+//     at Module._compile (internal/modules/cjs/loader.js:1101:27)   
+//     at Object.Module._extensions..js (internal/modules/cjs/loader.js:11
+// 57:10)
+//     at Module.load (internal/modules/cjs/loader.js:985:32)        
+//     at Function.Module._load (internal/modules/cjs/loader.js:878:14)
+
+//     at Function.executeUserEntryPoint [as runMain] (internal/modules/ru
+// n_main.js:71:12)
+//     at internal/main/run_main_module.js:17:47
