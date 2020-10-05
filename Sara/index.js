a90d5ee7fef0f90943317c1675c12c43744d3fa5
Feruj Ahammed <ferujahammed@gmail.com>
2020-10-05 22:13:53 +0200

    initial commit

--- Sara/index.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World"); 
-
-// ==> console.log("This is my first JavaScript file"); 
-
-// No it does not, it gives me SyntaxError
-
-// SyntaxErrror 
--- index.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello World");
-
-// console.log("This is my first js file");
-
-// The error is a synthax error!
