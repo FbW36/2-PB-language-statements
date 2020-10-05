fe57b2885a53c03b01966a8c7ddccb708b358bf6
Cindy <Punk.c@hotmail.com>
2020-10-05 14:14:44 +0200

    add 2 statements 1 is comment out

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+// console.log("This is my first jsfile");
+
+// Remove a parenthesis from the first statement and run the code. Does your statement print?
+// When removing the parenthesis , vscode is not able to run my code.
+
+//Is there an error? If so, what kind of error is it?
+//I get a very long error that states "unexpected string".
