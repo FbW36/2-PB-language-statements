6cb8c2e15286caf3648205fa6a09432d9f1b3fb1
Luke Dowling <lpdowling26@googlemail.com>
2020-10-05 13:52:54 +0200

    finished assignment

---
+++ luke/index.js
@@ -0,0 +1,9 @@
+// task 1/2
+console.log("Hello World");
+
+// task3
+// console.log("This is my first js file");
+
+// questions
+// task 1 /2
+//  it does not print and there is a syntax error: unexpected string
