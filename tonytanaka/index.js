35641c1fe79f30dec1e8a1f462f8a6843fe4e4a0
Tony Tanaka <tony.h.tanaka@gmail.com>
2020-10-05 14:00:52 +0200

    finished exercise

--- tonytanaka/index.js
+++ tonytanaka/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+console.log "Hello World!";
+// console.log("This is my first js file");
+
+// 1.
+// SyntaxError: Unexpected string
+//at Module._compile (internal/modules/cjs/loader.js:811:22)
+//at Object.Module._extensions..js (internal/modules/cjs/loader.js:879:10)
+//at Module.load (internal/modules/cjs/loader.js:731:32)
+//at Function.Module._load (internal/modules/cjs/loader.js:644:12)
+//at Function.Module.runMain (internal/modules/cjs/loader.js:931:10)
+//at internal/main/run_main_module.js:17:11
+
+// 2. a syntax error appear: Unexpected string
+
