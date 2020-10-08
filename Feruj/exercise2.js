4f588ae81453f0c5bb6270f35de7cb76648c7b7f
Feruj Ahammed <ferujahammed@gmail.com>
2020-10-08 14:40:12 +0200

    new

---
+++ Feruj/exercise2.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+//
+// console.log("This is my first js file");
+
+// If I remove a parenthesis from console.log the code throws an error
+// missing one side error is ==> SyntaxError: Invalid or unexpected token
+// missing both ==> SyntaxError: missing ) after argument list
