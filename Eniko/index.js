e7b6bdc7b0509967539164929cd16ad76b16da7b
Enikő Veszelka <encsooo@gmail.com>
2020-10-08 15:09:59 +0200

    changes were made

--- Eniko/index.js
+++ Eniko/index.js
@@ -1,18 +1,9 @@
 console.log(Hello World!);
 
-//console.log("This is my first JavaScript file.");
+console.log("This is my first JavaScript file.");
 
-//console.log(Hello World!);
-//^^^^^
+console.log("Hello World!");
 
-// SyntaxError: missing ) after argument list
-//     at wrapSafe (internal/modules/cjs/loader.js:1053:16)
-//     at Module._compile (internal/modules/cjs/loader.js:1101:27)   
-//     at Object.Module._extensions..js (internal/modules/cjs/loader.js:11
-// 57:10)
-//     at Module.load (internal/modules/cjs/loader.js:985:32)        
-//     at Function.Module._load (internal/modules/cjs/loader.js:878:14)
+// console.log(Hello World!);
+// It we leave out the "" we get an error message: console.log(Hello World!);, it is a SyntaxError.
 
-//     at Function.executeUserEntryPoint [as runMain] (internal/modules/ru
-// n_main.js:71:12)
-//     at internal/main/run_main_module.js:17:47
