0de62cfdab802f9c4a64c0db7ec5ea0708b1892c
limononoil <63930000+limononoil@users.noreply.github.com>
2020-10-19 21:14:50 +0200

    sorry for the delay

---
+++ Laura/index.js
@@ -0,0 +1,18 @@
+// 1. Create a JavaScript file and work from there.
+
+// 2. Print the phrase `Hello World`. Make sure to run your javascript file in node.
+console.log("Hello World"); // Hello World
+
+// 3. Print another phrase e.g. `This is my first js file`.
+console.log("This is my first js file"); // This is my first js file
+
+// 4. Comment out your second statement.
+// console.log("This is my first js file");
+
+// **Questions: Write your answers as a block comment in the js file**
+
+// 1. Remove a parenthesis from the first statement and run the code. Does your statement print?
+// No, it doesn't print
+
+// 2. Is there an error? If so, what kind of error is it?
+// A synthax error
